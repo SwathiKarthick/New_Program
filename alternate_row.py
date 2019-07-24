f2a31694b33ccb53cf07939b23c23590ccdf906c
SwathiKarthick <Swathinatarajan@@gmail.com>
2019-07-24 23:39:27 +0530

    Deleted my alternate_row program

--- alternate_row.py
+++
@@ -1,4 +0,0 @@
-#suppose you have a dataframe or table in python from that table select the alternate rows 
-values = input()
-numbers = [x for x in values.split(",") if int(x)%2!=0]
-print(",".join(numbers))
